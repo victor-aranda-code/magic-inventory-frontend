f53453c5a36fdb31138767d93ef24c53bbfa8090
Victor Aranda Lopez <victor.aranda500@gmail.com>
2026-02-08 12:51:24 +0100

    Advance search and DevOps

--- src/app/services/http-service.ts
+++ src/app/services/http-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
-import { map, Observable } from "rxjs";
+import { map, Observable, switchMap, of } from "rxjs";
+import { ImageUtils } from "../utils/image-utils";
 import { User } from "../models/User";
 import { RegisterRequest } from "../models/RegisterRequest";
 import { LoginRequest } from "../models/LoginRequest";
@@ -15,7 +16,7 @@ export class HttpService {
     private headers = new HttpHeaders();
     private params = new HttpParams();
     private responseType = 'json';
-    private INVENTORY_PREFIX = '/inventory';
+    private INVENTORY_PREFIX = '/api/v1/inventory';
     items: Item[] = [];
 
     constructor() { }
@@ -31,10 +32,10 @@ export class HttpService {
 
 
     registerUser(user: RegisterRequest): Observable<User> {
-        return this.http.post<User>(this.url + '/auth/register', user, this.createOptions());
+        return this.http.post<User>(this.url + '/api/v1/auth/register', user, this.createOptions());
     }
     login(user: LoginRequest): Observable<AuthResponse> {
-        return this.http.post<AuthResponse>(this.url + '/auth/login', user, this.createOptions());
+        return this.http.post<AuthResponse>(this.url + '/api/v1/auth/login', user, this.createOptions());
     }
 
     listAllItems(): Observable<Item[]> {
@@ -70,19 +71,21 @@ export class HttpService {
     editUser(user: User): Observable<User> {
         let options = this.createOptions();
         options.headers = options.headers.set('Content-Type', 'application/json');
-        return this.http.put<User>(this.url + '/user/' + user.id, JSON.stringify(user), options);
+        return this.http.put<User>(this.url + '/api/v1/auth/user/' + user.id, JSON.stringify(user), options);
     }
     deleteUserByAdmin(id: number): Observable<User> {
-        return this.http.delete<User>(this.url + '/user/' + id, this.createOptions());
+        return this.http.delete<User>(this.url + '/api/v1/auth/user/' + id, this.createOptions());
     }
 
-    addImage(itemId: string, image: string): Observable<Item> {
-        const options = {
-        };
-        var fd = new FormData();
-        fd.append('image', image);
-        fd.append('itemId', itemId);
-        return this.http.post<Item>(this.url + this.INVENTORY_PREFIX + '/images', fd, options);
+    addImage(itemId: string, image: any): Observable<Item> {
+        return ImageUtils.resizeImage(image, 320, 320).pipe(
+            switchMap(resizedBlob => {
+                var fd = new FormData();
+                fd.append('image', resizedBlob, 'image.jpg');
+                fd.append('itemId', itemId);
+                return this.http.post<Item>(this.url + this.INVENTORY_PREFIX + '/images', fd, {});
+            })
+        );
     }
     deleteImage(imageId: string): Observable<string> {
         return this.http.delete<string>(this.url + this.INVENTORY_PREFIX + '/images/' + imageId, this.createOptions());
@@ -96,5 +99,35 @@ export class HttpService {
             responseType: 'blob' as const
         };
         return this.http.get(this.url + this.INVENTORY_PREFIX + '/images/' + imageId, options);
+    }
+
+    autocompleteDetails(image: any, item: Item): Observable<Item> {
+        return ImageUtils.resizeImage(image, 320, 320).pipe(
+            switchMap(resizedBlob => {
+                var fd = new FormData();
+                fd.append('image', resizedBlob, 'image.jpg');
+                fd.append('item', new Blob([JSON.stringify(item)], { type: 'application/json' }));
+                return this.http.post<Item>(this.url + '/api/v1/ai/autocomplete', fd, {});
+            })
+        );
+    }
+
+    advancedSearch(image: any, searchTerm: string): Observable<Item[]> {
+        const imageResizer: Observable<Blob | null> = (image != null && image !== "")
+            ? ImageUtils.resizeImage(image, 320, 320)
+            : of(null);
+
+        return imageResizer.pipe(
+            switchMap((resizedBlob: Blob | null) => {
+                var fd = new FormData();
+                if (resizedBlob) {
+                    fd.append('image', resizedBlob, 'image.jpg');
+                }
+                if (searchTerm != null && searchTerm != "") {
+                    fd.append('query', searchTerm);
+                }
+                return this.http.post<Item[]>(this.url + '/api/v1/ai/search', fd, {});
+            })
+        );
     }
 }
